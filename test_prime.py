0b373776318bbe0702ec6a2527bee5472ac2a21d
Babuyera Yera <babuyera2019@example.com>
2025-07-25 16:20:14 -0400

    Format code with Black

--- test_prime.py
+++ test_prime.py
@@ -1,9 +1,11 @@
 from prime import is_prime
 
+
 def test_is_prime_true():
     assert is_prime(2) is True
     assert is_prime(3) is True
     assert is_prime(13) is True
+
 
 def test_is_prime_false():
     assert is_prime(0) is False
